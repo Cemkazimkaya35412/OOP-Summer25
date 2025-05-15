aee372470fb3f63378e686ee37b2bee815cfb915
Cemkazimkaya35412 <kayacem982@gmail.com>
2025-05-15 11:23:21 +0000

    add notebook for lecture 9

--- student_details.py
+++ student_details.py
@@ -1,7 +1,3 @@
-first_name = "Cem"
-last_name = "Kaya"
-index_number = "35412"
-nationality = "turkish"
-strating_date = 01\03\2025
-courses = "[comptuer engineering]"
-
+print("Cem Kazim Kaya")
+x = 6
+print()
